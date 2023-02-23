b0e41b0afefff529cc1f397af954d82563f256a3
imran hossain <mh.imranhossain@gmail.com>
2023-02-23 15:02:39 +0600

    sort before renaming

--- Tools/bulk_file_rename.py
+++ Tools/bulk_file_rename.py
@@ -1,67 +1,47 @@
 import os
+import natsort
 
-# Take input for the location of the files on the folder
-folder_path = input("Enter the path of the folder with the files to rename: ")
+folder_path = input("Folder Path: ")
+names_file_path = input("Text File Path: ")
 
-# Take input for the text file with all the names which will be used to rename all the files.
-names_file_path = input(
-    "Enter the path of the text file containing the new names: ")
-
-# Read the new names from the text file
 with open(names_file_path) as f:
     new_names = f.readlines()
-
-# Remove whitespace and newlines from the new names
 new_names = [name.strip() for name in new_names]
 
-# Check if the number of new names matches the number of files in the directory
 if len(new_names) != len(os.listdir(folder_path)):
-    print("Error: The number of new names in the text file does not match the number of files in the folder.")
+    print("⚠️ error: textfile line number != total files")
     exit()
 
-# Preview the renamed files
-print("Preview of renamed files:")
-for i, filename in enumerate(os.listdir(folder_path)):
-    # Get the file extension
+sorted_filenames = natsort.natsorted(os.listdir(folder_path))
+sorted_new_names = natsort.natsorted(new_names)
+
+print("Preview of renamed files: ")
+for i, filename in enumerate(sorted_filenames):
     ext = os.path.splitext(filename)[1]
-    # Construct the new filename with incrementing numbers
-    new_filename = f"{i+1:03d}_{new_names[i]}" + ext
-    # Print the preview
+    new_filename = f"{i+1:03d}_{sorted_new_names[i]}" + ext
     print(f"{filename} -> {new_filename}")
 
-# Check for conflicts
 conflicts = []
-for i, filename in enumerate(os.listdir(folder_path)):
-    # Get the file extension
+for i, filename in enumerate(sorted_filenames):
     ext = os.path.splitext(filename)[1]
-    # Construct the new filename with incrementing numbers
-    new_filename = f"{i+1:03d}_{new_names[i]}" + ext
-    # Check if the new filename already exists in the directory
+    new_filename = f"{i+1:03d}_{sorted_new_names[i]}" + ext
     if os.path.exists(os.path.join(folder_path, new_filename)):
         conflicts.append((filename, new_filename))
 
-# Show conflicts if any
 if conflicts:
-    print("Conflicts detected:")
+    print("Conflicts detected: ")
     for conflict in conflicts:
         print(f"{conflict[0]} -> {conflict[1]}")
 
-# Confirm with the user if they want to proceed with renaming
 confirmation = input("Do you want to proceed with renaming? (y/n): ")
 if confirmation.lower() != "y":
     exit()
 
-# Rename all the files
-for i, filename in enumerate(os.listdir(folder_path)):
-    # Get the file extension
+for i, filename in enumerate(sorted_filenames):
     ext = os.path.splitext(filename)[1]
-    # Construct the new filename with incrementing numbers
-    new_filename = f"{i+1:03d}_{new_names[i]}" + ext
-    # Rename the file
+    new_filename = f"{i+1:03d}_{sorted_new_names[i]}" + ext
     os.rename(os.path.join(folder_path, filename),
               os.path.join(folder_path, new_filename))
 
-# Success message
 print("Files successfully renamed.")
-# Open the renamed folder
 os.startfile(folder_path)
